b1f9d9c97766b793ef0e6412ff28590d942d263c
Gor777777777 <gorgoar@gmail.com>
2017-01-23 21:23:40 +0400

    I created a program for working with files

---
+++ DesctopManager/DesctopManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DesctopManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesctopManager")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bf485f57-e24a-4f69-9694-cd85a1f3b501")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DesctopManager/DesctopManager/ScanFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesctopManager
+{
+    public static class ScanFile
+    {
+        public static List<string> GetFileName(string path)
+        {
+            var scanList = new List<string>();
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\admin\Desktop\temp1");
+
+            foreach (var fi in di.GetFiles())
+            {
+                scanList.Add(fi.Name);
+            }
+
+            return scanList;
+        }
+
+        public static void RemoveUnnecessary(string path, List<string> saveFileName)
+        {
+            var scanList = new List<string>();
+            DirectoryInfo di = new DirectoryInfo(path);
+
+
+            foreach (var fi in di.GetFiles())
+            {
+                scanList.Add(fi.Name);
+            }
+
+            for (int i = scanList.Count - 1; i >= 0; i--)
+            {
+                foreach (var name2 in saveFileName)
+                {
+                    if (scanList[i] == name2)
+                    {
+                        scanList.Remove(scanList[i]);
+                    }
+                }
+            }
+
+            foreach (var item in scanList)
+            {
+                File.Delete(path + @"\" + item);
+            }
+        }
+    }
+}
