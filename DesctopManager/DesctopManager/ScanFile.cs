97982aa7013cffb3c3a7b60c98387da02586d56b
Gor777777777 <gorgoar@gmail.com>
2017-01-24 12:32:54 +0400

    Fixed bugs

--- DesctopManager/DesctopManager/ScanFile.cs
+++ DesctopManager/DesctopManager/ScanFile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace DesctopManager
 {
@@ -8,8 +9,12 @@ namespace DesctopManager
         public static List<string> GetFileName(string path)
         {
             var scanList = new List<string>();
-            DirectoryInfo di = new DirectoryInfo(@"C:\Users\admin\Desktop\temp1");
+            DirectoryInfo di = new DirectoryInfo(path);
 
+            foreach (var fi in di.GetDirectories())
+            {
+                scanList.Add(fi.Name);
+            }
             foreach (var fi in di.GetFiles())
             {
                 scanList.Add(fi.Name);
@@ -18,13 +23,17 @@ namespace DesctopManager
             return scanList;
         }
 
-        public static void RemoveUnnecessary(string path, List<string> saveFileName)
+        public static void RemoveUnnecessary(string path, string nameXmlFil)
         {
             var scanList = new List<string>();
             DirectoryInfo di = new DirectoryInfo(path);
-
+            var saveFileName = DeSerializeFromFile<List<string>>(nameXmlFil);
 
             foreach (var fi in di.GetFiles())
+            {
+                scanList.Add(fi.Name);
+            }
+            foreach (var fi in di.GetDirectories())
             {
                 scanList.Add(fi.Name);
             }
@@ -33,16 +42,44 @@ namespace DesctopManager
             {
                 foreach (var name2 in saveFileName)
                 {
-                    if (scanList[i] == name2)
+
+                    if (scanList.Count != 0 && scanList[i] == name2)
                     {
                         scanList.Remove(scanList[i]);
+                        break;
                     }
+
                 }
             }
 
             foreach (var item in scanList)
             {
                 File.Delete(path + @"\" + item);
+            }
+        }
+
+        public static void Serialize<T>(T source, string filename) where T : List<string>
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                serializer.Serialize(stream, source);
+            }
+        }
+
+        public static T DeSerializeFromFile<T>(string filename) where T : List<string>
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                using (var stream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    return (T)serializer.Deserialize(stream);
+                }
+            }
+            catch
+            {
+                return null;
             }
         }
     }
